ea45fdcf1adc7442162a3aacf449ae4cb15225b4
Jason Rossello <jrossellor1400@alumno.ipn.mx>
2020-03-23 12:23:11 -0600

    waf v1.1.2

--- wafd.py
+++ wafd.py
@@ -14,15 +14,16 @@ def initWAF():
     print("***** Web Application Firewall v1.0 *****")
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s_tcp1:
         s_tcp1.bind(('192.168.17.149', 80))
-        s_tcp1.listen(10)
+        s_tcp1.listen()
 
         while True:
             conn, addr = s_tcp1.accept()
             with conn:
                 data = conn.recv(1024)
                 with concurrent.futures.ThreadPoolExecutor() as executor:
-                    f1 = executor.submit(connHTTP, data)
-                    conn.send(f1.result())
+                    results = [executor.submit(connHTTP, data)]
+                    for f in concurrent.futures.as_completed(results):
+                        conn.sendall(f.result())
 
 
 if __name__ == '__main__':
