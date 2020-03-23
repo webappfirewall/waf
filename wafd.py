03d721c32da51f10dc62174c035e99b92b465e2d
Jason Rossello <jrossellor1400@alumno.ipn.mx>
2020-03-23 02:02:06 -0600

    waf v1.1.0

--- wafd.py
+++ wafd.py
@@ -1,12 +1,10 @@
 import socket
 import concurrent.futures
-# import threading
-# import signal
 
 
 def connHTTP(data):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s_tcp2:
-        s_tcp2.connect(('192.168.17.150', 8080))
+        s_tcp2.connect(('192.168.17.150', 80))
         s_tcp2.sendall(data)
         data2 = s_tcp2.recv(1024)
         return data2
@@ -24,10 +22,8 @@ def initWAF():
                 data = conn.recv(1024)
                 with concurrent.futures.ThreadPoolExecutor() as executor:
                     f1 = executor.submit(connHTTP, data)
-                    print(f1.result().decode())
-                # s_tcp1.sendall(f1.result())
+                    conn.sendall(f1.result())
 
 
 if __name__ == '__main__':
-    # signal.signal(signal.SIGPIPE, signal.SIG_DFL)
     initWAF()
