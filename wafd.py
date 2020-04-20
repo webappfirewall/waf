e72fbceef3972d8454752c4648c8461a14b21e68
Jason Rossello <jrossellor1400@alumno.ipn.mx>
2020-04-20 10:53:15 -0500

    waf v1.6.5

--- wafd.py
+++ wafd.py
@@ -55,8 +55,8 @@ def insertMongoDB(uri, addr, requestM, agent):
 def connHTTP(conn, addr):
     with conn:
         data = conn.recv(2048)
+        print(data)
         requestM = extractRequestM(data)
-        veredicto = '0'
 
         if requestM == "GET":
             uri = extractURI(data)
